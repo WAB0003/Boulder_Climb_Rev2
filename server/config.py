90eb4cacde18331a8f6822e86725f583f969daed
Bill Brown <bill.a.brown90@gmail.com>
2023-09-13 14:14:08 -0700

    Deployment

--- server/config.py
+++ server/config.py
@@ -3,13 +3,17 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] =  os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 app.secret_key = 'My Secret Key'
+
 
 db = SQLAlchemy()
 migrate = Migrate(app, db)
